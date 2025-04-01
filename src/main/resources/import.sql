74e363cb3b887f3f09a34d2345636814548c4a8a
Sebastian Arenas <jose.arenas01@uceva.edu.co>
2025-03-31 22:15:11 -0500

    feat: Datos de prueba en import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO facultad (id_decano, nombre) VALUES (22221,'Carlos');
+INSERT INTO facultad (id_decano, nombre) VALUES (55421,'Martin');
+INSERT INTO facultad (id_decano, nombre) VALUES (56061,'Jose');
+INSERT INTO facultad (id_decano, nombre) VALUES (7887,'Gabriel');

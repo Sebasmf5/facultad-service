589b732bdd53bbe1958dfad40361d9420b2a813a
Sebastian <139397006+Sebasmf5@users.noreply.github.com>
2025-05-21 12:05:12 -0500

    comunicacion con programa

--- src/main/java/co/edu/uceva/facultadservice/domain/repository/IFacultadRepository.java
+++ src/main/java/co/edu/uceva/facultadservice/domain/repository/IFacultadRepository.java
@@ -2,10 +2,13 @@ package co.edu.uceva.facultadservice.domain.repository;
 
 import co.edu.uceva.facultadservice.domain.model.Facultad;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
 Interfaz que hereda de JpaRepository para realizar las
 operaciones CRUD paginación y ordenamiento sobre la entidad Facultad
 **/
+
+@Repository
 public interface IFacultadRepository extends JpaRepository<Facultad, Long> {
 }
